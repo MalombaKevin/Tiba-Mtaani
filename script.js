9500f5e26ec61921283aa9cfd3ca7ac53ffca48d
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-03-25 10:24:35 +0300

    ft: js

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 $(document).ready(function() {
-    $("form").submit(function() {
+
+    function Search(location, service) {
+        (this.locationS = location),
+        (this.serviceS = service)
+    }
+
+    $("#form01").submit(function() {
+        event.preventDefault();
+        alert("Please login or sign up");
+    })
+
+
+    $("#form02").submit(function() {
         event.preventDefault()
-        $("#table").show()
+        var userSearch = new Search(
+            $("#loc").val(),
+            $("#serv").val()
+        )
+        if (userSearch.locationS === "" || userSearch.serviceS === "") {
+            alert("Please select both search inputs")
+        } else {
+            $("#table").show()
+            $("#servi").text(userSearch.serviceS);
+            $("#loca").text(userSearch.locationS)
+
+        }
+
     })
 })
